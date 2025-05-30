789981b7538265dcf70c7d3e5c4b225d6fdba6e8
Clayton Hickey <clayton@claytondoesthings.xyz>
2025-05-29 23:08:55 -0400

    Add US South

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,3 +1,4 @@
-wget https://download.geofabrik.de/north-america/us-northeast-latest.osm.pbf &&
-wget https://download.geofabrik.de/asia/japan/kyushu-latest.osm.pbf &&
-osmium cat us-northeast-latest.osm.pbf kyushu-latest.osm.pbf -o data.osm.pbf
+wget https://download.geofabrik.de/north-america/us-northeast-latest.osm.pbf -O us-northeast-latest.osm.pbf &&
+wget https://download.geofabrik.de/north-america/us-south-latest.osm.pbf -O us-south-latest.osm.pbf &&
+wget https://download.geofabrik.de/asia/japan/kyushu-latest.osm.pbf -O kyushu-latest.osm.pbf &&
+osmium cat us-northeast-latest.osm.pbf us-south-latest.osm.pbf kyushu-latest.osm.pbf -o data.osm.pbf --overwrite
